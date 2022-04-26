45a24d62e989790042941b716666fddf342e484d
Grace <cgracesharpe@gmail.com>
2022-04-26 17:21:37 -0400

    GD: trouble shooting service worker and files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -51,13 +51,16 @@ self.addEventListener('activate', function (e) {
 
 // waiting/idle  retrieve info from cache 'fetch'
 self.addEventListener('fetch', function (e) {
-    caches.match(e.request).then(function (request) {
-        if (request) {
-            console.log('responding with cache : ' + e.request.url)
-            return request
-        } else {
-            console.log('file is not cached, fetching : ' + e.request.url)
-            return fetch(e.request)
-        }
-    })
-});
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
